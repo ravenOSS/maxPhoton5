3490491590d12a1dc5f361475369bb8d0b5815ef
David Richards <35054853+ravenOSS@users.noreply.github.com>
2023-12-11 22:41:33 -0700

    Update to v5

--- src/maxPhoton5.cpp
+++ src/maxPhoton5.cpp
@@ -1,19 +1,16 @@
-#include "application.h"
-#line 1 "/Users/raventt/MBP_Projects/arduino/maxPhoton5/src/maxPhoton5.ino"
 /*
- * Project maxPhoton4
+ * Project maxPhoton5
  * Description: Interface Maxbotix serial sensor to Particle Photon or Arduino compatible
  * Author: David Richards / ravenIoT LLC
  * Date: May 22, 2019
- */
+ * With thanks to:
+ * https://github.com/nimasaj/Maxbotix_Ultrasonic/blob/master/Examples/Serial_read.ino
+ * for the unwitting help in seeing 'while' as a replacement for a 'for' loop
+*/
 
 // Using hardware UART on Photon, Electron, Fio
 // Maxbotix sensor with RS232 conversion to TTL with MAX3232
 
-void setup();
-void loop();
-uint16_t maxRead();
-#line 11 "/Users/raventt/MBP_Projects/arduino/maxPhoton5/src/maxPhoton5.ino"
 void setup()
 {
   Serial.begin(57600);
@@ -33,7 +30,6 @@ void setup()
 
 void loop()
 {
-  delay(100);
   uint16_t range = maxRead();
   Serial.print("Range: ");
   Serial.println(range);
@@ -50,43 +46,14 @@ uint8_t i = 0;                      // initialize counter
   while (Serial1.available()) {
     inChar = Serial1.read();        // continuously read sensor input in while loop
     if (inChar == 'R') {            // test if char == R for beginning data
-      Serial.println("Got an R");
+    // Serial.println("Got an R");  // Debug
       while (i < length) {
         charArray[i] = Serial1.read(); // assign input char to charArray index
-        Serial.print("char: ");
+        Serial.print("char: ");        // debug
         Serial.println(charArray[i]);
         i++;
       }
     }
   }
-
-  for (i = 0; i < 3; i++){
-  Serial.print("charArray: ");
-  Serial.println(charArray[i]);
-  }
   return atoi(charArray);            // extract integer range from char charArray
-}
-
-// void clearInputBuffer()             // Sensor continuously outputs data to built-in buffer
-// {                                   // This code is intended to empty sensor buffer
-//   // Serial1.flush();
-//   if (Serial1.available() != 0)
-//   {
-//     Serial1.read();
-//   }
-// }
-
-// int maxRead()
-// {
-//   while (Serial1.available() >= 0)
-//   {
-//     inChar = Serial1.read();
-//     if (inChar == 82)
-//     {
-//       Serial.println("Got 82");
-//       range = Serial1.parseInt();
-//       // Serial.println(range);
-//     }
-//   }
-//   return range;
-// }
+  }
